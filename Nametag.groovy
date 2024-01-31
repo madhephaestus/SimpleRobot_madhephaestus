0dce5f9ddd4e50c286a02c8e5d579db85c41825d
Kevin Harrington <kharrington@bancroftschool.org>
2024-01-31 15:02:08 -0500

    Creating workflow

--- Nametag.groovy
+++ Nametag.groovy
@@ -1,1 +1,36 @@
 // code here
+
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Cube
+
+CSG nametagBase = new Cube(70,30,3).toCSG()
+
+double distanceToBottom = nametagBase.getMinZ()
+
+nametagBase=nametagBase.movez(-distanceToBottom)
+			.toXMin()
+			.toYMin()
+
+double distancetoTop = nametagBase.getMaxZ()
+
+
+
+
+CSG name = CSG.text("Kevin yay so many things!", 2)
+				.movez(distancetoTop)
+
+double hackyScale = (nametagBase.getTotalX()-2)/name.getTotalX()  // FIXME this is a hack and needs to be calculated
+
+double yScale = (nametagBase.getTotalY()-2)/name.getTotalY()
+
+name=name		
+		.toXMin()	
+		.toYMin()	
+		.scalex(hackyScale)
+		.scaley(yScale)
+		.movex(1)
+		.movey(1)
+
+
+
+return [nametagBase, name]
