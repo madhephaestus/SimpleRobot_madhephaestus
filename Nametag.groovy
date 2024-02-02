9f187c35a18a1c305e9dd3bc19bcad947cd5f357
Kevin Harrington <kharrington@bancroftschool.org>
2024-02-02 13:29:31 -0500

    adding print bed and tag loopp
    #2

--- Nametag.groovy
+++ Nametag.groovy
@@ -2,8 +2,20 @@
 
 import eu.mihosoft.vrl.v3d.CSG
 import eu.mihosoft.vrl.v3d.Cube
+import eu.mihosoft.vrl.v3d.Cylinder
+import eu.mihosoft.vrl.v3d.parametrics.LengthParameter
 
-CSG nametagBase = new Cube(70,30,3).toCSG()
+LengthParameter nameTagHeightParam = new LengthParameter("Nametag Height", 30, [60,20])
+
+double baseX = 100
+double baseZ = 3
+double baseY =nameTagHeightParam.getMM()
+double textHeight =2
+double textToEdgeSpacing =1
+double ringDiameter = 20
+double holeDiameter=15
+
+CSG nametagBase = new Cube(baseX,baseY,baseZ).toCSG()
 
 double distanceToBottom = nametagBase.getMinZ()
 
@@ -16,23 +28,36 @@ double distancetoTop = nametagBase.getMaxZ()
 
 
 
-CSG name = CSG.text("Mr. Harrington", 2)
+CSG name = CSG.text("Mr. Harrington", textHeight)
 				.movez(distancetoTop)
 
-double xscale = (nametagBase.getTotalX()-2)/name.getTotalX()  
+double xscale = (nametagBase.getTotalX()-(textToEdgeSpacing*2))/name.getTotalX()  
 
-double yScale = (nametagBase.getTotalY()-2)/name.getTotalY()
+double yScale = (nametagBase.getTotalY()-(textToEdgeSpacing*2))/name.getTotalY()
 
 name=name		
 		.toXMin()	
 		.toYMin()	
 		.scalex(xscale)
 		.scaley(yScale)
-		.movex(1)
-		.movey(1)
-CSG tag = nametagBase.union(name)
-			.setName("KevinsNametag")
+		.movex(textToEdgeSpacing)
+		.movey(textToEdgeSpacing)
+CSG loop = new Cylinder(ringDiameter/2,baseZ).toCSG()	
+			.toXMax()
+			.movey(baseY/2)
+			
+CSG hole = new Cylinder(holeDiameter/2,baseZ).toCSG()
+			.movex(-ringDiameter/2)
+			.movey(baseY/2)
+		
+CSG tag = nametagBase
+			.union(loop)
+			.hull()
+			.difference(hole)
+			.union(name)
+			.setName("MrHarringtonNametag")
 
+tag.setParameter(nameTagHeightParam)
 
-return tag
+return [tag]
 
