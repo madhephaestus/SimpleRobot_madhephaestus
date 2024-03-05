b10a82f80f00cfdadba2700ca6d9cbbe9c957a96
Kevin Harrington <kharrington@bancroftschool.org>
2024-03-05 08:23:47 -0500

    adding a test script

--- TestKinematics.groovy
+++ TestKinematics.groovy
@@ -1,1 +1,23 @@
-// code here
+import com.neuronrobotics.sdk.addons.kinematics.DHParameterKinematics
+import com.neuronrobotics.sdk.addons.kinematics.MobileBase
+import com.neuronrobotics.sdk.addons.kinematics.math.RotationNR
+import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR
+import com.neuronrobotics.sdk.common.DeviceManager
+
+// Load the robot from the device manager
+MobileBase base =(MobileBase) DeviceManager.getSpecificDevice("SimpleRobot")
+if(base==null)
+	throw new RuntimeException("Please run the SimpleRobot.xml before running this script!")
+// Load the arm from the robot
+DHParameterKinematics arm = base.getAllDHChains().get(0)
+
+TransformNR target = new TransformNR(150,200,0,new RotationNR())
+// send the target to the arm with 0 seconds for transition
+arm.setDesiredTaskSpaceTransform(target, 0)
+
+Thread.sleep(500)
+
+TransformNR result = arm.getCurrentTaskSpaceTransform()
+
+println "Target = "+target
+println "Result = "+result
