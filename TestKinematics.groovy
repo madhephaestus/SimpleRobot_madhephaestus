6dbbc30b11384889f8d33b40a8ccdead3a23b7ac
Kevin Harrington <mad.hephaestus@gmail.com>
2024-03-27 10:15:35 -0400

    Update TestKinematics.groovy

--- TestKinematics.groovy
+++ TestKinematics.groovy
@@ -12,27 +12,27 @@ if(base==null)
 DHParameterKinematics arm = base.getAllDHChains().get(0)
 
 for (int i=0;i<150;i++){
-// Create a tip target of 150,200
-TransformNR target = new TransformNR(120-i,100,0,new RotationNR())
-// send the target to the arm with 0 seconds for transition
-arm.setDesiredTaskSpaceTransform(target, 0)
-// wait for the  arm to arrive at the location specified
-Thread.sleep(16)
-// Read the location of the tip after it finished moving
-TransformNR result = arm.getCurrentTaskSpaceTransform()
-
-// compute the tip error by subtracting result from the intended target
-double xError = target.getX() - result.getX()
-double yError = target.getY() - result.getY()
-
-// print the results
-println "Result (should be 0,0)= "+xError+","+yError
-
-if(Math.abs(xError)>0.001 || Math.abs(yError) > 0.001) {
-	println "FAIL, the  error is outside of the bounds"
-	println result
-}else {
-	println "Success!"
-}
+	// Create a tip target of 150,200
+	TransformNR target = new TransformNR(120-i,100,0,new RotationNR())
+	// send the target to the arm with 0 seconds for transition
+	arm.setDesiredTaskSpaceTransform(target, 0)
+	// wait for the  arm to arrive at the location specified
+	Thread.sleep(16)
+	// Read the location of the tip after it finished moving
+	TransformNR result = arm.getCurrentTaskSpaceTransform()
+	
+	// compute the tip error by subtracting result from the intended target
+	double xError = target.getX() - result.getX()
+	double yError = target.getY() - result.getY()
+	
+	// print the results
+	println "Result (should be 0,0)= "+xError+","+yError
+	
+	if(Math.abs(xError)>0.001 || Math.abs(yError) > 0.001) {
+		println "FAIL, the  error is outside of the bounds"
+		println result
+	}else {
+		println "Success!"
+	}
 
 }
